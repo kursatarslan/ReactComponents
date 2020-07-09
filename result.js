af83e9466f595a070c2f7d1af19dae7e4b270fdf
Hamzah Aldolaay <41927016+Haldolaay@users.noreply.github.com>
2020-07-08 21:23:46 -0700

    everything is changed

--- result.js
+++ result.js
@@ -1,25 +1,55 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./home.css";
-export default function Result(props) {
-  const [cards, setCards] = useState(props);
-  useEffect(() => {
-    setCards(props);
-  }, [props]);
-  return (
-    <div>
-      <div className="result">
-        {cards.result ? (
-          props.result.cards.map((item) => {
-            return (
-              <div className="resultValue">
+import Pagination from "@material-ui/lab/Pagination";
+import Container from "@material-ui/core/Container";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import PaginationItem from "@material-ui/lab/PaginationItem";
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    height: 140,
+    width: 100,
+  },
+  control: {
+    padding: theme.spacing(2),
+  },
+}));
+
+export default class Result extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  componentDidMount() {}
+  componentWillReceiveProps() {
+    console.log(this.props);
+  }
+  render() {
+    return (
+      <Grid container className={useStyles.root} spacing={10}>
+        <Grid item xs={12}>
+          <Grid container justify="center" spacing={2}>
+            {this.props.cards.map((item) => (
+              <div>
                 <img src={item.card_images[0].image_url_small} />
               </div>
-            );
-          })
-        ) : (
-          <div>there is nothing to show</div>
-        )}
-      </div>
-    </div>
-  );
+            ))}
+          </Grid>
+        </Grid>
+        <Pagination
+          count={this.props.cards.length}
+          color="primary"
+          renderItem={(item) => (
+            <PaginationItem
+              // component={Link}
+              // to={`/inbox${item.page === 1 ? '' : `?page=${item.page}`}`}
+              {...this.props.cards}
+            />
+          )}
+        />
+      </Grid>
+    );
+  }
 }
