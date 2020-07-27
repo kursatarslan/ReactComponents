b208c6db132299da4cda56a2bcd269bc0795fd38
Hamzah Aldolaay <41927016+Haldolaay@users.noreply.github.com>
2020-07-27 12:25:17 -0700

    steps to delete a project

--- redux.js
+++ redux.js
@@ -45,3 +45,85 @@ const store = createStore(reducer);
 // for the action add dispatch in your component
  this.props.dispatch({ type: '' });
 
+
+
+Here are the steps for deleting a project:
+
+
+• on ProjectDetails.js
+
+
+++ add an event handler like so:
+
+
+handleDelete = e => {
+        const { id } = this.props;
+        e.preventDefault();
+        this.props.deleteProject(id);
+        // you can push to dashboard after deleting...
+}
+
+
+++ add a button inside the component:
+
+
+<button onClick={this.handleDelete}>Delete</button>
+
+
+++ inside mapStateToProps, return id: id along with other states so you can grab the id and pass it to the deleteProject() action so your app has a reference on which project to delete:
+
+
+return {
+        id: id,
+        ...
+}
+
+
+const mapDistpacthToProps = dispatch => {
+    return {
+        deleteProject: (id) => dispatch(deleteProject(id))
+    }
+}
+
+
+export default compose(
+    connect(mapStateToProps, mapDistpacthToProps),
+    firestoreConnect([
+        { collection: 'projects' }
+    ])
+)(ProjectDetails);
+
+
+------
+
+
+• on your store actions --> projectActions.js
+
+
+export const deleteProject = id => {
+  return (dispatch, getState, { getFirebase, getFirestore }) => {
+    const firestore = getFirestore();
+    
+    firestore.collection('projects').doc(id)
+      .delete()
+      .then(() => {
+        dispatch({ type: 'DELETE_PROJECT', id })
+      }).catch(err => {
+        dispatch({ type: 'DELETE_PROJECT_ERROR', err })
+    })
+  }
+};
+
+
+------
+
+
+• on your store reducers --> projectReducer.js
+
+
+case 'DELETE_PROJECT':
+            console.log('delete project');
+            return state;
+        case 'DELETE_PROJECT_ERROR':
+            console.log('delete project error', 'state: ', state, 'action: ', action.project);
+            return state;
